c6c3069a9cd831cb6d08828857d57d815591236e
AbbyAdj <Abbyadjei9@gmail.com>
2025-04-26 13:24:46 +0100

    add conftest and test files

---
+++ tests/test_queries/get_queries/test_books_get_queries.py
@@ -0,0 +1,9 @@
+import pytest
+from app.src.utils.queries.get_queries import get_all_authors, get_all_genres,get_all_books,get_disliked_books,get_ongoing_books,get_completed_books
+
+
+class TestGetAllBooks:
+    @pytest.mark.it("jngjnj")
+    def test_get_all_books(self):
+        pass
+
